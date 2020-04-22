1336137843aa07d6bb94637b5751700861a82857
Marshellson <47588486+Marshellson@users.noreply.github.com>
2020-04-22 21:14:50 +0200

    initial com

---
+++ main.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+# @Time    : 4/22/2020 7:12 PM
+# @Author  : Wilson JIANG Yilun
+# @FileName: main.py
+# @Software: PyCharm
+
+
+def print_restaurant_info():
+    print("-" * 80,
+          "\n",
+          "-" * 30,
+          "RESTAURANT LIPSUM",
+          "-" * 30,
+          "\n",
+          "-" * 30,
+          " 15 RUE DES ECOLE",
+          "-" * 30,
+          "\n",
+          "-" * 79)
+
+
+print_restaurant_info()
+nom_client = input("Saissez voyre nom SVP")
+
+print_restaurant_info()
+print("Bonjour", nom_client, "! ", "\n")
+print("1. GESTION DE COMMANDE\n2. ")
+
+
+m = open("menu", 'r')
+menu_list = m.read().splitlines()
+
+exit()
