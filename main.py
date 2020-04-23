3f2ce1ea3572167549ad7d0c7aca8d7021924bb4
Marshellson <47588486+Marshellson@users.noreply.github.com>
2020-04-23 08:41:49 +0200

    Test
    Signed-off-by: Marshellson <47588486+Marshellson@users.noreply.github.com>

--- main.py
+++ main.py
@@ -19,6 +19,29 @@ def print_restaurant_info():
           "-" * 79)
 
 
+def text_save(content, filename, mode='w+'):
+    file = open(filename,mode)
+    for i in range(len(content)):
+        file.write(str(content[i])+'\n')
+    file.close()
+
+def text_read(file_name):
+
+    try:
+        file = open(file_name, 'r')
+    except IOError:
+        error = []
+        return error
+    data = []
+    content = file.read().splitlines()
+    for i in range(len(content)):
+        data[i] = content[i]
+
+    file.close()
+    return data
+
+
+
 print_restaurant_info()
 nom_client = input("Saissez voyre nom SVP")
 
