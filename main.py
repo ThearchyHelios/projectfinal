87084ded9bff808d4a03774ce6494b3f0f59f575
Marshellson <47588486+Marshellson@users.noreply.github.com>
2020-04-26 13:23:00 +0200

    Update
    Signed-off-by: Marshellson <47588486+Marshellson@users.noreply.github.com>

--- main.py
+++ main.py
@@ -60,12 +60,12 @@ def update_menu(menu):
 
 def add_menu():
     print("-" * 80)
-    print("Saissez des info comme 'TIAN DE LÉGUMES DU SOLEIL,Dessert,12' SVP")
+    print("Saissez des info comme 'TIAN DE LÉGUMES DU SOLEIL(Nom),Dessert(Type),12(Quantite)',5(Prix) SVP")
     print("-" * 80, "\n")
     menu = read_menu()
     user_add_menu = input("Ajouter ici:\n").strip()
     user_add_menu_list = user_add_menu.split(',')
-    if len(user_add_menu_list) == 3:
+    if len(user_add_menu_list) == 4:
         nom_menu = []
         for i in menu:
             nom_menu.append(i[0])
@@ -83,10 +83,35 @@ def del_menu():
     print("-" * 80)
     print("Saissez le nom du plat SVP")
     print("-" * 80, "\n")
+    menu_list = read_menu()
     nombre_menu = len(read_menu())
     del_quel_menu = input("Le nom du Plat: \n").strip()
+    for i in menu_list:
+        if i[0] == del_quel_menu:
+            print("Trouver ce plat:",
+                  "Nom: ", i[0], '\n',
+                  "Type: ", i[1], '\n',
+                  "Quantite: ", i[2], '\n',
+                  "prix", i[3])
+            request = input("Vous etes sure que vous voudrais del ce plat?(oui/non)")
+            if request == "oui":
+                index_del_menu = menu_list.index(i)
+                del menu_list[index_del_menu]
+                update_menu(menu_list)
+                print("Successful")
+            else:
+                print("Exit")
 
 
+def show_menu():
+    print("-" * 80)
+    menu = read_menu()
+    print("Nom du plat".ljust(50), "Type".ljust(10), "Quantite".ljust(10), "Prix".ljust(5))
+    for i in menu:
+        print(i[0].ljust(50), i[1].ljust(10), i[2].ljust(10), i[3].ljust(5))
 
-add_menu()
-del_menu()
+
+def change_menu(nom_du_menu):
+
+
+show_menu()
